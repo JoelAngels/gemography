fa3027294e11f96c9a5059e9e2ca78f06158c9af
JoelAngels <joelbaraka4325@gmail.com>
2021-05-26 15:26:48 +0300

    Structered the App component, created the Loading component that when fetching data from the github api it displays it the data when loading is set of false

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import Loading from "./Components/Loading";
+import axios from "axios";
+import moment from "moment";
+
+const App = () => {
+  const [state, setState] = useState({
+    repo: [],
+    error: "",
+    page: 1,
+    loading: true,
+  });
+
+  useEffect(() => {
+    const loadRepo = () => {
+      const { page, repo } = state;
+
+      const DATE_30_DAYS_BEFORE = moment()
+        .subtract(30, "days")
+        .format("YYYY-MM-DD");
+
+      // Getting the data from Github API
+
+      axios
+        .get(
+          ` https://api.github.com/search/repositories?q=created:>${DATE_30_DAYS_BEFORE}&sort=stars&order=desc&page=${page} `
+        )
+
+        .then((resp) => {
+          setState((oldState) => ({
+            ...oldState,
+            repo: [...repo, ...resp.data.items], // here when scrolling, Repo get updated instantly
+            loading: false,
+          }));
+        })
+
+        .catch((error) => {
+          setState((oldState) => ({
+            ...oldState,
+            error: error,
+            loading: false,
+          }));
+        });
+    };
+    loadRepo();
+  }, [state]);
+
+  return (
+    <div>
+      <Loading />
+    </div>
+  );
+};
+
+export default App;
